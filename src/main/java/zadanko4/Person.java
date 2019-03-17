c21c1f0aebbdc31816f1bc1dde582b3f4d21ca19
Krzysztof Kramarz <krzysztof.kramarz@silvermedia.pl>
2019-03-18 00:25:40 +0100

    zadanko 4 flatMaap

---
+++ src/main/java/zadanko4/Person.java
@@ -0,0 +1,56 @@
+package zadanko4;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author krzysztof.kramarz
+ */
+public class Person
+{
+   private String name;
+   private Long id;
+   private Set<String> zdobyteSzczyty = new HashSet<>();
+
+   public Person(String name, Long id)
+   {
+      this.name = name;
+      this.id = id;
+
+   }
+
+   public String getName()
+   {
+      return name;
+   }
+
+   public Long getId()
+   {
+      return id;
+   }
+
+   public Set<String> getZdobyteSzczyty()
+   {
+      return zdobyteSzczyty;
+   }
+
+   public void setSzczyt(String szczyt)
+   {
+
+      if (zdobyteSzczyty == null)
+      {
+         zdobyteSzczyty = new HashSet<>();
+      }
+      zdobyteSzczyty.add(szczyt);
+   }
+
+   @Override
+   public String toString()
+   {
+      return "Person{" +
+            "name='" + name + '\'' +
+            ", id=" + id +
+            ", zdobyteSzczyty=" + zdobyteSzczyty +
+            '}';
+   }
+}
